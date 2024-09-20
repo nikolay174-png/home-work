581851182e5728ebd61de7169b01181123a1697d
nikolay174-png <nikolay.savenyuk@gmail.com>
2024-09-20 12:26:33 +0500

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,9 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3st program
+print(2 * 2 + 2 or 2 * 2 + 2)
+print(2 * 2 + 2 and 2 * 2 == 2)
+# 4th program
+print(int((float("123.456"))*10)%10)
