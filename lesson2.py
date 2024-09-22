c441aee7757b6ce49c6adde2422bf3d818ab79f3
nikolay174-png <nikolay.savenyuk@gmail.com>
2024-09-22 19:05:19 +0500

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+n_of_homework = 12
+n_of_hours = 1.5
+course = 'Python'
+time_for_one_work = n_of_hours / n_of_homework
+print(course,",""n_of_homework",n_of_homework,",""n_of_hours",n_of_hours,",""time_for_one_work",time_for_one_work)
